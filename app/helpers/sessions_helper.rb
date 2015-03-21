c7a6097079fe7c445b7e714a7815f51e51dfb20d
mayuu <mayusola2527@gmail.com>
2015-03-21 13:56:11 +0000

    Finish sign in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,7 +10,6 @@ module SessionsHelper
   def signed_in?
     !current_user.nil?
   end
-  
   def current_user=(user)
     @current_user = user
   end
@@ -19,7 +18,6 @@ module SessionsHelper
     remember_token = User.encrypt(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
   end
-
   def sign_out
     self.current_user = nil
     cookies.delete(:remember_token)
