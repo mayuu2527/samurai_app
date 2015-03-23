6832b3ab582be177ac1dfd9d866c112b0154f11f
mayuu <mayusola2527@gmail.com>
2015-03-23 06:12:05 +0000

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def index
     @users = User.paginate(page: params[:page])
   end
-
+  
   def show
     @user = User.find(params[:id])
   end
@@ -16,16 +16,6 @@ class UsersController < ApplicationController
   end
 
   def edit
-  end
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
-    else
-      render 'new'
-    end
   end
 
   def update
